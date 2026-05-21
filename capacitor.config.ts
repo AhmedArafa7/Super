e9674e8a3bf04765fd7643d624bba295bc4e90dc
AhmedArafa7 <mo1999382@gmail.com>
2026-05-21 14:50:41 +0300

    fix: change appId to unique, and sign release build with debug key for seamless installation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nexusai.app',
+  appId: 'com.ahmedarafa.nexusai',
   appName: 'NexusAI',
   webDir: 'public',
   server: {
